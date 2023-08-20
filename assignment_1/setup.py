a904960ef4b91d32f6e82df25e640eab3b14dd0a
Daniel Duberg <danielduberg@gmail.com>
2023-08-21 00:37:25 +0200

    Added the first assignment as well as some shared resource files.

---
+++ assignment_1/setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'assignment_1'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append((os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))))
+data_files.append(('share/' + package_name + '/rviz', ['rviz/kitti.rviz']))
+data_files.append(('share/' + package_name + '/rviz', ['rviz/turtlebot_simulation.rviz']))
+data_files.append(('share/' + package_name + '/rviz', ['rviz/turtlebot_real.rviz']))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Daniel Duberg',
+    maintainer_email='dduberg@kth.se',
+    description='WASP Autonomous Systems Course Assignment 1',
+    license='TODO: License declaration',
+    tests_require=[],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ wasp_autonomous_systems/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'wasp_autonomous_systems'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append((os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.[wbt]*'))))
+data_files.append(('share/' + package_name + '/resource', ['resource/ros2control.yaml']))
+data_files.append(('share/' + package_name + '/mesh', ['mesh/car.dae']))
+data_files.append(('share/' + package_name + '/mesh', ['mesh/car.jpg']))
+data_files.append(('share/' + package_name + '/urdf', ['urdf/turtlebot_webots.urdf']))
+data_files.append(('share/' + package_name + '/urdf', ['urdf/car.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Daniel Duberg',
+    maintainer_email='dduberg@kth.se',
+    description='Resources for the WASP Autonomous Systems course',
+    license='TODO: License declaration',
+    tests_require=[],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
