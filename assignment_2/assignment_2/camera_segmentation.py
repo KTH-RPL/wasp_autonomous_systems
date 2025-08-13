07a9f2bc9f152a1b839b64967b487b104aff98da
danielduberg <danielduberg@gmail.com>
2025-08-13 14:24:16 +0200

    Added compressed topic and changed to YOLO 11 from 8

--- assignment_2/assignment_2/camera_segmentation.py
+++ assignment_2/assignment_2/camera_segmentation.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge
 
 from rcl_interfaces.msg import ParameterDescriptor
@@ -15,40 +15,64 @@ class CameraSegmentation(Node):
         super().__init__('camera_segmentation')
 
         # Load a pretrained model (recommended for training)
-        self._model = YOLO("yolov8n-seg.pt")
+        self._model = YOLO("yolo11n-seg.pt")
+        # self._model.export(format="openvino", imgsz=640, int8=True, data="coco128.yaml")
+
+        # self._model = YOLO("yolo11n-seg_int8_openvino_model/", task="segment")
 
         # Bridge to convert between ROS and OpenCV
         self._cv_bridge = CvBridge()
 
         # Publisher
         self._seg_pub = self.create_publisher(
-            Image, 'segmentation/image_raw', 10)
+            Image, '/image/segmentation', 10)
+        self._seg_compressed_pub = self.create_publisher(
+            CompressedImage, '/image/compressed/segmentation', 10)
 
         # Device parameter
         self.declare_parameter('device', '', ParameterDescriptor(
             description='Select device to run YOLOv8 on, use \'cpu\' for cpu, \'0\', \'1\', ... for GPU X'))
 
         # Subscribe to image topic
-        self.create_subscription(Image, '/image_raw', self.image_callback, 10)
+        self.create_subscription(Image, '/image', self.image_callback, 1)
+        self.create_subscription(
+            CompressedImage, '/image/compressed', self.image_compressed_callback, 1)
 
     def image_callback(self, image: Image):
+        if 0 == self._seg_pub.get_subscription_count() and 0 == self._seg_compressed_pub.get_subscription_count():
+            return
+
         # Convert from ROS to OpenCV
-        cv_image = self._cv_bridge.imgmsg_to_cv2(
-            image, desired_encoding='rgb8')
+        cv_image = self._cv_bridge.imgmsg_to_cv2(image, desired_encoding='rgb8')
 
-        device = self.get_parameter(
-            'device').get_parameter_value().string_value
+        self.process_image(cv_image, image.header)
 
-        result = self._model(cv_image, verbose=False, device=device)
+    def image_compressed_callback(self, image: CompressedImage):
+        if 0 == self._seg_pub.get_subscription_count() and 0 == self._seg_compressed_pub.get_subscription_count():
+            return
+
+        # Convert from ROS to OpenCV
+        cv_image = self._cv_bridge.compressed_imgmsg_to_cv2(image, desired_encoding='rgb8')
+
+        self.process_image(cv_image, image.header)
+
+    def process_image(self, image, header):
+        device = self.get_parameter('device').get_parameter_value().string_value
+
+        result = self._model(image, verbose=False, device=device)
 
         for r in result:
             # Plot a BGR numpy array of predictions
             im_array = r.plot()
             # Convert from OpenCV to ROS
-            image = self._cv_bridge.cv2_to_imgmsg(
-                im_array[..., ::-1], encoding="rgb8")
-            image.header = image.header
-            self._seg_pub.publish(image)
+            if 0 < self._seg_pub.get_subscription_count():
+                ret = self._cv_bridge.cv2_to_imgmsg(im_array[..., ::-1], encoding="rgb8")
+                ret.header = header
+                self._seg_pub.publish(ret)
+            if 0 < self._seg_compressed_pub.get_subscription_count():
+                ret = self._cv_bridge.cv2_to_compressed_imgmsg(im_array[..., ::-1])
+                ret.header = header
+                self._seg_compressed_pub.publish(ret)
 
 
 def main():
